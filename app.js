2e2b87a96e42125ea44d64a4dc7ae31511e40150
ryan dyson <dysonryan6@gmail.com>
2025-09-06 10:35:05 +0800

    push

--- app.js
+++ app.js
@@ -1,48 +1,50 @@
-const express = require('express');
-const morganBody = require('morgan-body');
+const express = require("express");
+const morganBody = require("morgan-body");
 const PORT = process.env.PORT || 5000;
 
 const app = express().use(express.json());
-morganBody(app, { noColors: process.env.NODE_ENV === 'production' });
-
+morganBody(app, { noColors: process.env.NODE_ENV === "production" });
 
 /**
-  * @param {Array} customer 
-  * @param {Array} concerts
-  * @returns {string}
-  */
+ * @param {Array} customer
+ * @param {Array} concerts
+ * @returns {string}
+ * push comment
+ */
 function findBestConcert(customer, concerts, priority) {
   function dist([x0, y0], [x1, y1]) {
-    return Math.sqrt((x0-x1)**2 + (y0-y1)**2)
+    return Math.sqrt((x0 - x1) ** 2 + (y0 - y1) ** 2);
   }
 
   function distToLatency(dist) {
-    if (dist <= 1) return 30
-    if (dist <= 4) return 20
-    if (dist <= 9) return 10
-    return 0
+    if (dist <= 1) return 30;
+    if (dist <= 4) return 20;
+    if (dist <= 9) return 10;
+    return 0;
   }
 
-  // Get the distances 
+  // Get the distances
   /** @type {[string, number][]} */
-  const values = concerts.map(({name, booking_center_location}) => {
-    const pointsFromLatency = distToLatency(dist(customer.location, booking_center_location))
-    const pointsFromCC = priority[customer.credit_card] == name ? 50 : 0
-    return [
-      name, pointsFromLatency+pointsFromCC
-    ]
-  })
+  const values = concerts.map(({ name, booking_center_location }) => {
+    const pointsFromLatency = distToLatency(
+      dist(customer.location, booking_center_location)
+    );
+    const pointsFromCC = priority[customer.credit_card] == name ? 50 : 0;
+    return [name, pointsFromLatency + pointsFromCC];
+  });
 
-  const max = values.reduce((prev, cur) => prev[1] > cur[1] ? prev : cur) 
-  return max[0]
+  const max = values.reduce((prev, cur) => (prev[1] > cur[1] ? prev : cur));
+  return max[0];
 }
 
 app.post("/ticketing-agent", (req, res) => {
-  const {customers, concerts, priority} = req.body;
-  res.json(Object.fromEntries(
-    customers.map(x => [x.name, findBestConcert(x, concerts, priority)]
-    )))
-})
+  const { customers, concerts, priority } = req.body;
+  res.json(
+    Object.fromEntries(
+      customers.map((x) => [x.name, findBestConcert(x, concerts, priority)])
+    )
+  );
+});
 
 app
   .post("/square", (req, res) => {
