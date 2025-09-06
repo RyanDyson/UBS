3eb8b2129ef6d8708080b25a7fe5f449db7b259c
ryan dyson <dysonryan6@gmail.com>
2025-09-06 16:42:50 +0800

    update trivia

--- app.js
+++ app.js
@@ -113,7 +113,7 @@ app.post("/ticketing-agent", (req, res) => {
 
 app.get("/trivia", (req, res) => {
   res.json({
-    answers: [4, 1, 2, 2, 3, 4, 4, 5, 4],
+    answers: [4, 1, 2, 2, 3, 4, 4, 5, 4, 3, 3, 3, 4, 1, 2, 2, 1, 2, 2, 2, 1],
   });
 });
 
